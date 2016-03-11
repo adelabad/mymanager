a7dd37726c26f7da212449a7792f5c0c18f1f7be
alireza <aprahmani94@gmail.com>
2016-03-11 11:30:35 +0330

    new thread class

--- refresh.py
+++ refresh.py
@@ -2,9 +2,8 @@ import  threading
 import time
 class refresh(threading.Thread):
 
-    def __init__(self, name = ''):
-        threading.Thread.__init__(self, name = name)
-
+    def __init__(self, name=''):
+        threading.Thread.__init__(self, name=name)
 
 
     def run(self):
@@ -15,5 +14,17 @@ class refresh(threading.Thread):
                 time.sleep(3)
             except:
                 break
+
+
+
+class new_window(threading.Thread):
+
+    def __init__(self, func, name=''):
+        threading.Thread.__init__(self, name=name)
+        self.func = func
+
+    def run(self):
+        self.func()
+
 
 #need to complete
